212528a4600bc407801f6f8bdc6cc8adf0e5259f
abelsanxd <abelsan4665@gmail.com>
2024-04-08 20:43:11 -0300

    agregamos el componente informacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
 function App() {
-  return <h1>Prueba de titulo</h1>;
+  return (
+    <>
+      <main className="container my-5">
+        <h1>Prueba de titulo</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti
+          recusandae, voluptas nesciunt, in laboriosam suscipit explicabo
+          molestiae ab ullam blanditiis nostrum voluptatibus amet laborum
+          voluptates voluptatum ducimus sequi animi. Dolorum.
+        </p>
+      </main>
+      ;
+    </>
+  );
 }
 
 export default App;
