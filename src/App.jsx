346619107fdee2efc31e8520af68cff5c8142dd9
abelsanxd <abelsan4665@gmail.com>
2024-04-08 21:23:58 -0300

    agregamos el maquetado del ccomponente contador

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Informacion from "./compontens/Informacion";
+import Contador from "./compontens/Contador";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <main className="container my-5">
         <h1>Primer proyecto con react</h1>
         <Informacion comision='c89i'></Informacion>
+        <Contador/>
       </main>
       ;
     </>
