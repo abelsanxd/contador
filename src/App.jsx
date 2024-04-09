759df575c0033b09c17f42ab837453360933636d
abelsanxd <abelsan4665@gmail.com>
2024-04-08 21:12:44 -0300

    enviamos props al componente e informacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import Informacion from "./compontens/Informacion";
 
 function App() {
   return (
     <>
       <main className="container my-5">
-        <h1>Prueba de titulo</h1>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti
-          recusandae, voluptas nesciunt, in laboriosam suscipit explicabo
-          molestiae ab ullam blanditiis nostrum voluptatibus amet laborum
-          voluptates voluptatum ducimus sequi animi. Dolorum.
-        </p>
+        <h1>Primer proyecto con react</h1>
+        <Informacion comision='c89i'></Informacion>
       </main>
       ;
     </>
--- src/compontens/Informacion.jsx
+++ src/compontens/Informacion.jsx
@@ -1,4 +1,5 @@
-const Informacion = () => {
+import PropTypes from 'prop-types';
+const Informacion = (props) => {
   return (
     <>
       <h2>Conceptos de react</h2>
@@ -9,10 +10,15 @@ const Informacion = () => {
         representan diferentes partes de la interfaz de usuario. React utiliza
         un paradigma de programación declarativa, lo que significa que los
         desarrolladores describen cómo debería ser la interfaz de usuario y
-        React se encarga de manejar los cambios de manera eficiente.
+        React se encarga de manejar los cambios de manera eficiente. Comision:
+        {props.comision}
       </p>
     </>
   );
 };
+
+Informacion.propTypes = {
+    comision: PropTypes.string.isRequired, // Validación para la prop 'comision'
+  };
 
 export default Informacion;
