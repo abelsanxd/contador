25385fb025f9918091ef71fd9d27b28f4557df9e
abelsanxd <abelsan4665@gmail.com>
2024-04-10 20:43:56 -0300

    agregamos la logica restar

--- src/compontens/Contador.jsx
+++ src/compontens/Contador.jsx
@@ -1,35 +1,40 @@
 import { useState } from "react";
 
-useState
+useState;
 
 const Contador = () => {
-    //aqui agrego la logica del componente
+  //aqui agrego la logica del componente
 
-    //  let numero = 1;
-    //aqui me creo el state
+  //  let numero = 1;
+  //aqui me creo el state
 
-    const [numero, setNumero] = useState(1);
-    
-     const sumar = () => {
-        setNumero(numero + 1)
-        console.log(numero)
-     }
-     const restar = () => {
-        setNumero(numero - 1)
-        
-     }
-    return (
+  const [numero, setNumero] = useState(1);
 
-        //aqui puedo agregar un poquito de logica
+  //  const sumar = () => {
+  //     setNumero(numero + 1)
+  //  }
+  //   const restar = () => {
+  //     setNumero(numero - 1);
+  //   };
+  return (
+    //aqui puedo agregar un poquito de logica
 
-        <section className='text-center'>
-            {/* aqui va la zona del maquetado */}
-            <h2 >Contador</h2>
-            <p>{numero}</p>
-            <button className='btn btn-primary me-2' onClick={sumar}>+1</button>
-            <button className='btn btn-danger' onClick={restar}>-1</button>
-        </section>
-    );
+    <section className="text-center">
+      {/* aqui va la zona del maquetado */}
+      <h2>Contador</h2>
+      <p>{numero}</p>
+      <button
+        className="btn btn-primary me-2"
+        onClick={() => setNumero(numero + 1)}
+      >
+        +1
+      </button>
+      <button className="btn btn-danger" onClick={() => setNumero(numero - 1)}>
+        -1
+      </button>
+      {/* tarea:agregar la funcion restar el valor de state numero */}
+    </section>
+  );
 };
 
 export default Contador;
